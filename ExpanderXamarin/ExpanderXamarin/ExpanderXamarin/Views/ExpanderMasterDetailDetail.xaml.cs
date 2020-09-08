3d9d0706ecb5165ef7e07e6d162102462de47d10
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-09-08 15:46:59 +0530

    XAMARIN-36352 - Sample and screen shot added.

---
+++ ExpanderXamarin/ExpanderXamarin/ExpanderXamarin/Views/ExpanderMasterDetailDetail.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExpanderXamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExpanderMasterDetailDetail : ContentPage
+    {
+        public ExpanderMasterDetailDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
